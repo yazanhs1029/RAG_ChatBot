412b998a6d7ef88d1a4c993b76c405557997923c
yazanhs1029 <160542974+yazanhs1029@users.noreply.github.com>
2025-09-28 20:16:32 +0300

    Delete Back-End/models directory

--- Back-End/models/UserText.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const UserInput = mongoose.Schema({
-  Embedding: { type: [Number], required: true },
-  chunk: { type: string, required: true },
-});
-const UserTextModel = mongoose.model("UserText", UserInput);
-
-export default UserTextModel;
